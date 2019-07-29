c28edd38a02c7359156940f57b3bb569f8978bce
harryliuhao <harryliuhao@hotmail.com>
2019-07-28 21:53:21 -0400

    initial type

---
+++ type-1.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 21:24:43 2019
+
+@author: Hao
+"""
+
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jul 28 17:02:10 2019
+
+@author: Hao
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import random as random
+
+plt.close('all')
+
+
+def f(t):
+    coeff=random.random()
+    return np.exp(-t*coeff) * np.cos(2*np.pi*t)
+
+
+
+t1 = np.arange(0.0, 5.0, 0.1)
+t2 = np.arange(0.0, 5.0, 0.02)
+
+
+#plt.figure(1)
+#plt.subplot(211)
+for x in range (6):
+    plt.plot(t2, f(t2), random.randint(1,10))
+
+
+
+plt.show()
