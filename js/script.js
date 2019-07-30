e55bfd5f4f35f4d9cee5452bcab49b1f5147c75c
harryliuhao <harryliuhao@hotmail.com>
2019-07-29 21:51:12 -0400

    js template

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+
+
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+
+    function draw(event) {
+      var canvas = document.getElementById('canvas');
+      if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 50, 50);
+
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+      }
+    }
+
+    // Unobtrusive event binding
+    document.getElementById("main-content")
+      .addEventListener("onload", draw);
+
+  }
+);
